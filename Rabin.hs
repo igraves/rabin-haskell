1ac7662d96ee67fa6cd5ba2b54a2ac862d610275
Ian Graves <thegravian@gmail.com>
2011-12-05 17:59:40 -0600

    more work

--- Rabin.hs
+++ Rabin.hs
@@ -5,9 +5,11 @@ import Primes
 import Data.Word
 import Data.Bits
 import Data.List
+import Debug.Trace
 import qualified Data.Binary as B
 import qualified Data.Binary.Get
 import qualified Data.ByteString.Lazy as BS
+import System.Random
 
 
 getKeys :: IO (Integer,Integer)
@@ -67,4 +69,37 @@ eeuclid a b = let start = (0,0,0,1,1,0,a,b)
                                         y1' = y'
                                     in
                                       eproc (x',y',x1',x2',y1',y2',a',b')
-                                      
+
+msqrt :: Integer -> Integer -> IO (Integer,Integer)
+msqrt p a = do
+              b <- getB
+              let f = a^2 - (b*a) + a
+              let r x = (fexp x (p+1 `div` 2) f)
+              return (r a,-1*(r a))
+
+            
+    where
+      getB = do
+              genEntry <- getStdRandom (randomR (1::Integer,p))
+              if ((genEntry^2 - 4*a) `div` p) - p == -1
+                then return genEntry 
+                else trace (show $ (genEntry)) getB
+
+--Fast modulo exponentiation
+--From the Handbook of Applied Cryptography
+--g(x) -> k -> f(a) -> (\x -> g(x)^k `mod` f(x))
+fexp :: Integer  -> Integer ->  Integer -> Integer  
+fexp g 0 f = 1
+fexp g k f = let (z:zs) = (tobits k)
+                 s = if z then g else 1
+              in
+                fexp' g zs s f
+    where            
+          fexp' g [] s f = s
+          fexp' g (k:ks) s f = let g' = (g)^2 `mod` f
+                                in if k -- if this bit is set
+                                 then fexp' g' ks (((g') * (s)) `mod` (f)) f
+                                 else fexp' g' ks s f
+          --convert integral to bits
+          tobits 0 = []
+          tobits i = testBit i 0 : (tobits $ shiftR i 1)
